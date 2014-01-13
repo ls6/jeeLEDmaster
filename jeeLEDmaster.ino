0d41b7d4c3e7d5e0b30f673571f963d8293ddf1f
Lukasz Szostek <lukasz.szostek@gmail.com>
2014-01-13 10:20:58 +0100

    Testing color transition.

--- jeeLEDmaster.ino
+++ jeeLEDmaster.ino
@@ -5,23 +5,35 @@
 
 #define SERIAL_BAUD 57600
 #define VERSION 1
+#define COLOR_CHANGE_PERIOD 1000
+#define TARGET_HUE 36
 
 CRGB leds[1];
+MilliTimer timer1;
+byte currentHue = 6;
 
-static void handleInput (char c) {
+void handleInput (char c) {
   switch (c) {
     case '1':
       Serial.print("got: ");
       Serial.println(c);
-      leds[0] = CHSV(224, 187, 255);
-      Serial.print("red: ");
-      Serial.println(leds[0].r);
-      Serial.print("green: ");
-      Serial.println(leds[0].g);
-      Serial.print("blue: ");
-      Serial.println(leds[0].b);
       break;
   };
+}
+
+static byte newHue () {
+  return (currentHue++);
+}
+
+void printRGB(byte hue) {
+  leds[0] = CHSV(hue, 255, 255);
+  Serial.print(hue);
+  Serial.print("->");
+  Serial.print(leds[0].r);
+  Serial.print(" ");
+  Serial.print(leds[0].g);
+  Serial.print(" ");
+  Serial.println(leds[0].b);
 }
 
 void setup() {
@@ -32,6 +44,13 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available())
-  handleInput(Serial.read());
+  if (Serial.available()) {
+    handleInput(Serial.read());
+  };
+
+  if (currentHue <= TARGET_HUE) {
+    if (timer1.poll(COLOR_CHANGE_PERIOD)) {
+      printRGB(newHue());
+    };
+  };
 }
